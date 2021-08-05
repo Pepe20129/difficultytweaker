98a1e4c104bc474a68418ffc2907c2394aa0f46e
Pepe20129 <72659707+Pepe20129@users.noreply.github.com>
2021-08-05 15:32:19 +0200

    Fix config file reset on startup

--- src/main/java/io/github/pepe20129/difficultytweaker/Reference.java
+++ src/main/java/io/github/pepe20129/difficultytweaker/Reference.java
@@ -11,13 +11,12 @@ public class Reference {
     private static ModConfig config = new ModConfig();
 
     static {
-        File configFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), "difficultytweaker.toml");;
+        File configFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), "difficultytweaker.toml");
         try {
-            if (configFile.exists()) {
-
+            if (configFile.exists())
                 config = new Toml().read(configFile).to(ModConfig.class);
-            }
-            new TomlWriter().write(new ModConfig(), configFile);
+            else
+                new TomlWriter().write(new ModConfig(), configFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
